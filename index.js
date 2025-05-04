5b6ff6b50bdac1b9107d3c5c0614666cce801b82
joinsachinarya <sachinkarya333@gmail.com>
2025-05-05 03:09:56 +0530

    revamped codebase

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function toggleTheme() {
   document.body.classList.toggle('light-theme');
-  localStorage.setItem('theme', document.body.classList.contains('light-theme') ? 'light' : 'dark');
-  document.querySelector("#theme-toggle-icon").src = document.body.classList.contains("light-theme") ? "public/moon.svg" : "public/sun.svg";
+  let isLightTheme = document.body.classList.contains('light-theme');
+  localStorage.setItem('theme', isLightTheme ? 'light' : 'dark');
+  document.querySelector("#theme-toggle-icon").src = isLightTheme ? "public/moon.svg" : "public/sun.svg";
+  
+  document.querySelector("header").classList.toggle("bg-[#ffffff]", isLightTheme);
+  document.querySelector("header").classList.toggle("bg-[#0a0a0a]", !isLightTheme);
 }
